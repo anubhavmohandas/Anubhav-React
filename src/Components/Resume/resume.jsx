b79a4fcaf337430c0ae4749178cfb0a7cd0f97a9
Anubhav Mohandas <anubhav.manav147@gmail.com>
2022-09-29 14:01:37 +0530

    build

---
+++ src/Components/Resume/resume.jsx
@@ -0,0 +1,44 @@
+import "./resume.css";
+// import {
+//     StyleSheet, Button, View, SafeAreaView,
+//     Text, Alert
+// } from 'react-native';
+
+
+const resume = () => {
+    return(
+        // <view style={styles.container}>
+            <button
+                title="Download Resume"
+                        a href= "./Anubhav.pdf"
+            />
+           // </view>
+    )
+}
+
+export default resume;
+
+/*/import React from 'react';
+  
+// Import the essential components from react-native
+import {
+    StyleSheet, Button, View, SafeAreaView,
+    Text, Alert
+} from 'react-native';
+  
+// Function for creating button
+export default function App() {
+    return (
+        <View style={styles.container}>
+  
+            // Create a button
+            <Button
+  
+                // Some properties given to Button
+                title="Geeks"
+                onPress={() => Alert.alert(
+                    'Its GeeksforGeeks !')}
+            />
+        </View>
+    );
+} */
